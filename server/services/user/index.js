bbcd908587fbe8630f4b09edd416d1f634032f9e
David Welsh <welshdav@msu.edu>
2019-02-20 16:48:45 -0600

    Create index.js

---
+++ server/services/user/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const service = require('feathers-sequelize');
+const user = require('./user-model');
+const hooks = require('./hooks');
+
+module.exports = function(){
+  const app = this;
+
+  const options = {
+    Model: user(app.get('sequelize'))};
+
+  // Initialize our services with any options it requires
+  app.use('/users', service(options));
+
+  // Get our initialize services to that we can bind hooks
+  const userService = app.service('/users');
+
+  // Set up our before hooks
+  userService.before(hooks.before);
+
+  // Set up our after hooks
+  userService.after(hooks.after);
+};
