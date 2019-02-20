877689e611e70234e23a360eb697e3ed4b95e9ff
David Welsh <welshdav@msu.edu>
2019-02-20 16:04:20 -0600

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -4,3 +4,11 @@ exports.myHook = function(options) {
     console.log('My custom global hook ran. Feathers is awesome!');
   };
 };
+
+const app = require('./app');
+const port = app.get('port');
+const server = app.listen(port);
+
+server.on('listening', () =>
+  console.log(`Feathers application started on ${app.get('host')}:${port}`)
+);
