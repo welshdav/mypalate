8a56f6f406c779c7f0aee73aa04003d744e3ad14
David Welsh <welshdav@msu.edu>
2019-02-20 16:23:38 -0600

    Create index.js

---
+++ server/services/follower/hooks/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const globalHooks = require('../../../hooks');
+const hooks = require('feathers-hooks');
+import updateRank from './update-rank';
+
+exports.before = {
+  all: [],
+  find: [],
+  get: [],
+  create: [],
+  update: [],
+  patch: [],
+  remove: []
+};
+
+exports.after = {
+  all: [],
+  find: [],
+  get: [],
+  create: [
+    updateRank
+  ],
+  update: [],
+  patch: [],
+  remove: [
+    updateRank
+  ]
+};
