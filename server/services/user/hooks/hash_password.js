04d0092e36493e3f5fa7673ca85908008de95a3e
David Welsh <welshdav@msu.edu>
2019-02-20 16:46:57 -0600

    Create hash_password.js

---
+++ server/services/user/hooks/hash_password.js
@@ -0,0 +1,43 @@
+import errors from 'feathers-errors';
+import bcrypt from 'bcryptjs';
+
+
+const defaults = { passwordField: 'password' };
+
+export default function (options = {}) {
+    return function (hook) {
+        if (hook.type !== 'before') {
+            throw new Error(`The 'hashPassword' hook should only be used as a 'before' hook.`);
+        }
+
+        options = Object.assign({}, defaults, hook.app.get('auth'), options);
+
+        const crypto = options.bcrypt || bcrypt;
+
+        if (hook.data === undefined) {
+            return hook;
+        }
+
+        const password = hook.data[options.passwordField];
+
+        if (password === undefined) {
+            return hook;
+        }
+        else {
+            return new Promise(function (resolve, reject) {
+                crypto.genSalt(10, function (error, salt) {
+                    crypto.hash(password, salt, function (error, hash) {
+                        if (error) {
+                            return reject(error);
+                        }
+
+                        hook.data[options.passwordField] = hash;
+                        resolve(hook);
+                    });
+                });
+            });
+        }
+
+
+    };
+}
