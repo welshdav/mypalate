78c7c83f2851e94c8671b799e96621f00237db73
David Welsh <welshdav@msu.edu>
2019-02-20 16:48:11 -0600

    Create index.js

---
+++ server/services/user/hooks/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const globalHooks = require('../../../hooks');
+const hooks = require('feathers-hooks');
+const auth = require('feathers-authentication').hooks;
+import hashPassword from './hash-password';
+
+exports.before = {
+  all: [],
+  find: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToAuthenticated()
+  ],
+  get: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToAuthenticated(),
+    auth.restrictToOwner({ ownerField: 'id' })
+  ],
+  create: [
+    hashPassword()
+  ],
+  update: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToAuthenticated(),
+    auth.restrictToOwner({ ownerField: 'id' })
+  ],
+  patch: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToAuthenticated(),
+    auth.restrictToOwner({ ownerField: 'id' }),
+    hashPassword()
+  ],
+  remove: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToAuthenticated(),
+    auth.restrictToOwner({ ownerField: 'id' })
+  ]
+};
+
+exports.after = {
+  all: [hooks.remove('password')],
+  find: [],
+  get: [],
+  create: [],
+  update: [],
+  patch: [],
+  remove: []
+};
